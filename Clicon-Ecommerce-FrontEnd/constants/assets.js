d1b943ba27ce8acc2fd3f9f0abf008b22e86d3f8
Muhammad Ali <alimaqbool2122@gmail.com>
2025-12-26 20:17:43 +0500

    update code

---
+++ Clicon-Ecommerce-FrontEnd/constants/assets.js
@@ -0,0 +1,224 @@
+// All Assest..
+import add_mobile_1 from "../public/image/add/add-mobile-1.webp";
+import airpod_02 from "../public/image/add/airpod-02.webp";
+import airpods from "../public/image/add/airpods.webp";
+import apple_accessories from "../public/image/add/apple-accessories.webp";
+import mobile from "../public/image/add/mobile.webp";
+import soundbox from "../public/image/add/soundbox.webp";
+import x_box from "../public/image/banner/x-box.webp";
+import cproduct_9 from "../public/image/category/product-9.webp";
+import cproduct_10 from "../public/image/category/product-10.webp";
+import cproduct_11 from "../public/image/category/product-11.webp";
+import cproduct_12 from "../public/image/category/product-12.webp";
+import cproduct_13 from "../public/image/category/product-13.webp";
+import cproduct_14 from "../public/image/category/product-14.webp";
+import d_mobile from "../public/image/featured-itme/d-mobile.webp";
+import f_i_1 from "../public/image/featured-itme/f-i-1.webp";
+import f_i_2 from "../public/image/featured-itme/f-i-2.webp";
+import f_i_3 from "../public/image/featured-itme/f-i-3.webp";
+import feature_1 from "../public/image/features/feature-1.webp";
+import feature_2 from "../public/image/features/feature-2.webp";
+import feature_3 from "../public/image/features/feature-3.webp";
+import feature_4 from "../public/image/features/feature-4.webp";
+import amaazon from "../public/image/logo/amaazon.webp";
+import googel from "../public/image/logo/googel.webp";
+import philips from "../public/image/logo/philips.webp";
+import samsung from "../public/image/logo/samsung.webp";
+import toshiba from "../public/image/logo/toshiba.webp";
+import logo_primary from "../public/image/logo/logo-primary.webp";
+import logo_white from "../public/image/logo/logo-white.webp";
+import man from "../public/image/logo/man.webp";
+import rus from "../public/image/logo/rus.webp";
+import usa from "../public/image/logo/usa.webp";
+import newsleter_1 from "../public/image/modal/newsleter-1.webp";
+import news_1 from "../public/image/news/1.webp";
+import news_2 from "../public/image/news/2.webp";
+import news_3 from "../public/image/news/3.webp";
+import favicon from "../public/image/favicon.webp";
+import payment_method from "../public/image/payment-method.webp";
+import gallery_01 from "../public/image/product/gallery/01.webp";
+import gallery_02 from "../public/image/product/gallery/02.webp";
+import gallery_03 from "../public/image/product/gallery/03.webp";
+import gallery_04 from "../public/image/product/gallery/04.webp";
+import gallery_05 from "../public/image/product/gallery/05.webp";
+import gallery_06 from "../public/image/product/gallery/06.webp";
+import apple from "../public/image/product/apple.webp";
+import mac from "../public/image/product/mac.webp";
+import product_1 from "../public/image/product/product-1.webp";
+import product_2 from "../public/image/product/product-2.webp";
+import product_3 from "../public/image/product/product-3.webp";
+import product_4 from "../public/image/product/product-4.webp";
+import product_5 from "../public/image/product/product-5.webp";
+import product_6 from "../public/image/product/product-6.webp";
+import product_7 from "../public/image/product/product-7.webp";
+import product_8 from "../public/image/product/product-8.webp";
+import product_9 from "../public/image/product/product-9.webp";
+import product_10 from "../public/image/product/product-10.webp";
+import product_11 from "../public/image/product/product-11.webp";
+import product_12 from "../public/image/product/product-12.webp";
+import product_13 from "../public/image/product/product-13.webp";
+import product_14 from "../public/image/product/product-14.webp";
+import product_15 from "../public/image/product/product-15.webp";
+import product_16 from "../public/image/product/product-16.webp";
+import product_17 from "../public/image/product/product-17.webp";
+import product_18 from "../public/image/product/product-18.webp";
+import product_19 from "../public/image/product/product-19.webp";
+import product_20 from "../public/image/product/product-20.webp";
+import product_21 from "../public/image/product/product-21.webp";
+import product_22 from "../public/image/product/product-22.webp";
+import product_23 from "../public/image/product/product-23.webp";
+import product_24 from "../public/image/product/product-24.webp";
+import product_25 from "../public/image/product/product-25.webp";
+import product_26 from "../public/image/product/product-26.webp";
+import product_27 from "../public/image/product/product-27.webp";
+import product_28 from "../public/image/product/product-28.webp";
+import product_29 from "../public/image/product/product-29.webp";
+import product_30 from "../public/image/product/product-30.webp";
+import product_31 from "../public/image/product/product-31.webp";
+import product_32 from "../public/image/product/product-32.webp";
+import product_33 from "../public/image/product/product-33.webp";
+import product_34 from "../public/image/product/product-34.webp";
+import product_35 from "../public/image/product/product-35.webp";
+import product_36 from "../public/image/product/product-36.webp";
+import product_37 from "../public/image/product/product-37.webp";
+import product_38 from "../public/image/product/product-38.webp";
+import product_39 from "../public/image/product/product-39.webp";
+import product_40 from "../public/image/product/product-40.webp";
+import product_41 from "../public/image/product/product-41.webp";
+import product_42 from "../public/image/product/product-42.webp";
+import product_43 from "../public/image/product/product-43.webp";
+import product_44 from "../public/image/product/product-44.webp";
+import ps5 from "../public/image/product/ps5.webp";
+import Cart_Black from "../public/image/svg/Cart-Black.svg";
+import Cart_White from "../public/image/svg/Cart-White.svg";
+import Eye_Black from "../public/image/svg/Eye-Black.svg";
+import Eye_White from "../public/image/svg/Eye-White.svg";
+import Heart_Black from "../public/image/svg/Heart-Black.svg";
+import Heart_White from "../public/image/svg/Heart-White.svg";
+import user_svg from "../public/image/svg/User.svg";
+import Heart from "../public/image/svg/Heart.svg";
+import Eye from "../public/image/svg/Eye.svg";
+import CaretDown from "../public/image/svg/CaretDown.svg";
+import House from "../public/image/svg/House.svg";
+import caretRight from "../public/image/svg/CaretRight.svg";
+import CaretDown_Gray from "../public/image/svg/CaretDown_Gray.svg";
+import x from "../public/image/svg/X.svg";
+import Ellipse from "../public/image/svg/Ellipse.svg";
+import apple_watch from "../public/image/add/apple-watch.png";
+import watch_7 from "../public/image/add/watch-7.png";
+import product_45 from "../public/image/product/product-45.webp";
+import product_46 from "../public/image/product/product-46.webp";
+import Check from "../public/image/svg/Check.svg";
+
+export const assets = {
+  Check,
+  product_46,
+  product_45,
+  watch_7,
+  apple_watch,
+  Ellipse,
+  x,
+  CaretDown_Gray,
+  House,
+  CaretDown,
+  caretRight,
+  Heart,
+  Eye,
+  Cart_Black,
+  Cart_White,
+  Eye_Black,
+  Eye_White,
+  Heart_Black,
+  Heart_White,
+  user_svg,
+  add_mobile_1,
+  airpod_02,
+  airpods,
+  apple_accessories,
+  mobile,
+  soundbox,
+  x_box,
+  cproduct_9,
+  cproduct_10,
+  cproduct_11,
+  cproduct_12,
+  cproduct_13,
+  cproduct_14,
+  d_mobile,
+  f_i_1,
+  f_i_2,
+  f_i_3,
+  feature_1,
+  feature_2,
+  feature_3,
+  feature_4,
+  amaazon,
+  googel,
+  logo_primary,
+  logo_white,
+  man,
+  philips,
+  rus,
+  samsung,
+  toshiba,
+  usa,
+  newsleter_1,
+  news_1,
+  news_2,
+  news_3,
+  favicon,
+  payment_method,
+  gallery_01,
+  gallery_02,
+  gallery_03,
+  gallery_04,
+  gallery_05,
+  gallery_06,
+  apple,
+  mac,
+  product_1,
+  product_2,
+  product_3,
+  product_4,
+  product_5,
+  product_6,
+  product_7,
+  product_8,
+  product_9,
+  product_10,
+  product_11,
+  product_12,
+  product_13,
+  product_14,
+  product_15,
+  product_16,
+  product_17,
+  product_18,
+  product_19,
+  product_20,
+  product_21,
+  product_22,
+  product_23,
+  product_24,
+  product_25,
+  product_26,
+  product_27,
+  product_28,
+  product_29,
+  product_30,
+  product_31,
+  product_32,
+  product_33,
+  product_34,
+  product_35,
+  product_36,
+  product_37,
+  product_38,
+  product_39,
+  product_40,
+  product_41,
+  product_42,
+  product_43,
+  product_44,
+  ps5,
+};
---
+++ Clicon-Ecommerce-FrontEnd/constants/routes.js
@@ -0,0 +1,7 @@
+const ROUTES = {
+  HOME: "/",
+  SHOP: "/shop",
+  PRODUCT_DETAILS: (id) => `/product-detail/${id}`,
+};
+
+export default ROUTES;
